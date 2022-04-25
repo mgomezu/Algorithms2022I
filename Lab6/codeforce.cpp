ea4542b055683ae12b81de80e66d4f7eae8e7912
mgomezu <49196702+mgomezu@users.noreply.github.com>
2022-04-24 23:33:25 -0500

    Create codeforce.cpp

---
+++ Lab6/codeforce.cpp
@@ -0,0 +1,23 @@
+{
+    using namespace std;
+    int t;
+    string s;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>s;
+        if(s.size()%2==0){
+            string left = s.substr(0,s.size()/2);
+            string right = s.substr(s.size()/2,(s.size())/2);
+            if (left==right){
+                cout<<"YES";
+            }else{
+                cout<<"NO";
+            }
+        }else{
+            cout<<"NO";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
